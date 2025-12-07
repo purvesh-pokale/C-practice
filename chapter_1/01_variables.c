6cba74ad4cfcee1e5056a178d0db13536ec5a5ca
purvesh-pokale <pokalepurvesh2003@gmail.com>
2025-12-07 17:12:18 +0530

    01

--- chapter_1/01_variables.c
+++ chapter_1/01_variables.c
@@ -3,7 +3,7 @@
 int main() {
     int a; // variable declaration
     a = 6; // variable initialization
-    printf("%d", a);
+    printf("%d", a); //%d=Print this value as an integer
     
     return 0;
 }
