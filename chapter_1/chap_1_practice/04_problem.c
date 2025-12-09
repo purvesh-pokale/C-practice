ef6dcbc5239aef4189c04b1c06209b7d71ac0bfb
purvesh-pokale <pokalepurvesh2003@gmail.com>
2025-12-09 22:31:02 +0530

    simple interes

--- chapter_1/chap_1_practice/04_problem.c
+++ chapter_1/chap_1_practice/04_problem.c
@@ -4,6 +4,18 @@
 
 int main()
  {
+    int p ;
+    printf("Enter the vale of principal: ");
+    scanf("%d", &p);
     
+    float t , r ;
+    printf("Enter the vale of time: ");
+    scanf("%f", &t);
+
+    printf("Enter the vale of rate of interest: ");
+    scanf("%f", &r);
+
+    printf("The simple interest is %f " , (p*r*t)/100 );
+
     return 0;
 }
