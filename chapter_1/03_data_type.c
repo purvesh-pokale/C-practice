fe56e2163aa6e82eb950520b9abf001b62925816
purvesh-pokale <pokalepurvesh2003@gmail.com>
2025-12-09 10:48:28 +0530

    data type

---
+++ chapter_1/03_data_type.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    a = 1;
+
+    float b;
+    b = 1.4;
+
+    char c;
+    c = 'a';
+    
+    printf("the value of a is %d\n", a);
+    printf("the value of b is %f\n", b);
+    printf("the value of c is %c\n", c);
+
+    return 0;
+}
