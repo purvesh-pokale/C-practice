8b1883f6460ee562743a8e95a4ea887df8358923
purvesh-pokale <pokalepurvesh2003@gmail.com>
2025-12-09 22:19:55 +0530

    simple interes

---
+++ chapter_1/chap_1_practice/04_problem.c
@@ -0,0 +1,9 @@
+//4. Write a program to calculate simple interest for a set of values representing principal, number of years and rate of interest.
+
+#include <stdio.h>
+
+int main()
+ {
+    
+    return 0;
+}
