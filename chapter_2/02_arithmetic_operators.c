7781b3a1c8bb5cd361e370c391fdfb9cc7abc6c4
purvesh-pokale <pokalepurvesh2003@gmail.com>
2025-12-11 11:25:51 +0530

    arithmetic_operators

---
+++ chapter_2/02_arithmetic_operators.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+ {
+    int a=4;
+    int b=3;
+    int c = a+b;
+    printf("the valeu of a is %d and value of b is %d and sum is %d ", a , b , c);
+
+    
+    return 0;
+}
