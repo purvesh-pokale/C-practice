87d2a776bb7128344eb9e75e797492bad9f3335b
purvesh-pokale <pokalepurvesh2003@gmail.com>
2025-12-11 11:31:09 +0530

    arithmetic_operators

--- chapter_2/02_arithmetic_operators.c
+++ chapter_2/02_arithmetic_operators.c
@@ -5,8 +5,12 @@ int main()
     int a=4;
     int b=3;
     int c = a+b;
-    printf("the valeu of a is %d and value of b is %d and sum is %d ", a , b , c);
-
     
+    printf("the valeu of a is %d and value of b is %d and sum is %d \n", a , b , c);
+
+    // modulus operator is used to get the remainder
+
+    printf("The remainder when a is divided by b is: %d" , a%b);
+
     return 0;
 }
