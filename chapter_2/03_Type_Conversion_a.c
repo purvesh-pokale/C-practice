4e61b9b15c2a36a17978843b9b728a01c0370b9e
purvesh-pokale <pokalepurvesh2003@gmail.com>
2025-12-13 17:36:26 +0530

    arithmetic_operators(float and int → int)

---
+++ chapter_2/03_Type_Conversion_a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+ {
+    float a=9.0;
+    int b=2;
+    float c= a/b;  //(float and int → int) OR (float and float -> float)
+
+    printf("The value of a/b is %f ", c);
+
+    return 0;
+}
