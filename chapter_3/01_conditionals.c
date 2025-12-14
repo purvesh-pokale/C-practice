a7e3ecb0949249ea3b11063cbd971c475a76f408
purvesh-pokale <pokalepurvesh2003@gmail.com>
2025-12-14 17:10:41 +0530

    if statement

---
+++ chapter_3/01_conditionals.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+ {
+    int age = 15;
+
+
+    if(age>10)  //here only if your age is grater than 10 than only inside curly bracket will run
+    {
+
+        printf("Your age is greater then 10");
+    }
+
+    return 0;
+}
