2df80002cad5936ed8bb81af4e757e90368b45dd
purvesh-pokale <pokalepurvesh2003@gmail.com>
2025-12-17 17:18:19 +0530

    if-else ladder

---
+++ chapter_3/03_if_elese_ladder.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+ {
+    int age;
+    printf("Enter your age:");
+    scanf("%d", &age);
+
+    if(age>10)
+    {
+
+        printf("Your age is greater then 10");
+    }
+    else if (age==10)
+    {
+        printf("Your age is equal to 10");
+    }
+    else 
+    {
+        printf("Your age is not greater then 10");
+    }
+
+    return 0;
+}
