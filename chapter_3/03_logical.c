cc9e4e8a24321bc84960eccec7ceee8fb440c13d
purvesh-pokale <pokalepurvesh2003@gmail.com>
2025-12-20 22:11:56 +0530

    use of logical operators

---
+++ chapter_3/03_logical.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+ {
+    int a=1 ; int b=1;
+    
+    printf("the value of a and b is %d\n" , a&&b);
+    printf("the value of a and b is %d\n" , a||b);
+    printf("the value of not a is %d\n" , !a);
+    
+    if(a&&b){
+        printf("Both are true\n");
+    }
+
+    if(a){
+        if(b){
+            printf("Both are true");
+        }
+    }
+    
+    return 0;
+
+}
